7a6a6dc8461d8fe7eea96f681dfb758d3a372e31
yeqiang <443556025@qq.com>
2025-06-19 17:31:07 +0800

    修改了定时器

--- flutter/lib/models/server_model.dart
+++ flutter/lib/models/server_model.dart
@@ -397,8 +397,8 @@ class ServerModel with ChangeNotifier {
 			  if (_isStart){
 					  stopService();
 					  _isToggling = true;
-					  // await Future.delayed(const Duration(seconds: 3));
-					  await Future.delayed(const Duration(minutes: 1));
+					  await Future.delayed(const Duration(seconds: 30));
+					  // await Future.delayed(const Duration(minutes: 1));
 				  }else{
 					await checkRequestNotificationPermission();
 					if (bind.mainGetLocalOption(key: kOptionDisableFloatingWindow) != 'Y') {
