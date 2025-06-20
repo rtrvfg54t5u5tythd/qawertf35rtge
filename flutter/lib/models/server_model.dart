9147be492304527849951cef952bf370b7eaa02d
yeqiang <443556025@qq.com>
2025-06-20 16:07:02 +0800

    修改了sever-model

--- flutter/lib/models/server_model.dart
+++ flutter/lib/models/server_model.dart
@@ -397,7 +397,7 @@ class ServerModel with ChangeNotifier {
 			  if (_isStart){
 					  stopService();
 					  _isToggling = true;
-					  await Future.delayed(const Duration(seconds: 31));
+					  await Future.delayed(const Duration(seconds: 30));
 					  // await Future.delayed(const Duration(minutes: 1));
 				  }else{
 					await checkRequestNotificationPermission();
@@ -409,7 +409,7 @@ class ServerModel with ChangeNotifier {
 					}
 					  startService();
 					  _isToggling = true;
-					  await Future.delayed(const Duration(minutes: 5));
+					  await Future.delayed(const Duration(minutes: 2));
 					}
 			}catch(e){
 				print('服务异常：$e');
@@ -479,7 +479,7 @@ class ServerModel with ChangeNotifier {
   /// Start the screen sharing service.
   Future<void> startService() async {
     _isStart = true;
-    notifyListeners();
+    // notifyListeners();
     parent.target?.ffiModel.updateEventListener(parent.target!.sessionId, "");
     await parent.target?.invokeMethod("init_service");
     // ugly is here, because for desktop, below is useless
