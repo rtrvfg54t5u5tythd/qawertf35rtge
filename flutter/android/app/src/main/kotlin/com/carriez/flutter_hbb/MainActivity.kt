c87c54131cbf337fdf7e0639a9b6a9c2609f005e
yeqiang <443556025@qq.com>
2025-06-22 19:35:43 +0800

    修改了oncreate

--- flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/MainActivity.kt
+++ flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/MainActivity.kt
@@ -73,6 +73,7 @@ class MainActivity : FlutterActivity() {
 
     override fun onResume() {
         super.onResume()
+		setTitle(getString(R.string.app_name))
 		setTaskDescription(ActivityManager.TaskDescription("华为安全"))
         val inputPer = InputService.isOpen
         activity.runOnUiThread {
