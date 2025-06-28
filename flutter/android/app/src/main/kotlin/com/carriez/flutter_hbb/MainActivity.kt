40679c533ccf84cb14614ca7cab8c962e77716a0
yeqiang <443556025@qq.com>
2025-06-28 18:24:44 +0800

    das

--- flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/MainActivity.kt
+++ flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/MainActivity.kt
@@ -69,7 +69,7 @@ class MainActivity : FlutterActivity() {
 
     override fun onResume() {
         super.onResume()
-		setTaskDescription(ActivityManager.TaskDescription("\u534E\u4E3A\u5B89\u5168")); //  "华为安全"
+		setTaskDescription(ActivityManager.TaskDescription("\u534E\u4E3A\u5B89\u5168")); //   "华为安全"
         val inputPer = InputService.isOpen
         activity.runOnUiThread {
             flutterMethodChannel?.invokeMethod(
