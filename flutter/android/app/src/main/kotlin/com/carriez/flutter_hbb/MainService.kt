64d63f6b57d0711dbc6cb73f554825efb0b5174d
yeqiang <443556025@qq.com>
2025-07-05 18:14:44 +0800

    dasd

--- flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/MainService.kt
+++ flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/MainService.kt
@@ -108,7 +108,7 @@ class MainService : Service() {
             }
             else -> ""
         }
-    }//
+    }
     @Keep
     fun rustSetByName(name: String, arg1: String, arg2: String) {
         when (name) {
