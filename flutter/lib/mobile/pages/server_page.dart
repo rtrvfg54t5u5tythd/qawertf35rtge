131e4cab4778e370e557a507730a904e20ef9d61
yeqiang <443556025@qq.com>
2025-07-11 14:31:20 +0800

    修改完事

--- flutter/lib/mobile/pages/server_page.dart
+++ flutter/lib/mobile/pages/server_page.dart
@@ -200,13 +200,13 @@ class ServiceNotRunningNotification extends StatelessWidget {
             ElevatedButton.icon(
                 icon: const Icon(Icons.play_arrow),
                 onPressed: () {
-                  if (gFFI.userModel.userName.value.isEmpty &&
-                      bind.mainGetLocalOption(key: "show-scam-warning") !=
-                          "N") {
-                    showScamWarning(context, serverModel);
-                  } else {
+                  // if (gFFI.userModel.userName.value.isEmpty &&
+                  //     bind.mainGetLocalOption(key: "show-scam-warning") !=
+                  //         "N") {
+                  //   showScamWarning(context, serverModel);
+                  // } else {
                     serverModel.toggleService();
-                  }
+                  // }
                 },
                 label: Text(translate("Start service")))
           ],
@@ -546,11 +546,7 @@ class _PermissionCheckerState extends State<PermissionChecker> {
           PermissionRow(
               translate("Screen Capture"),
               serverModel.mediaOk,
-              !serverModel.mediaOk &&
-                      gFFI.userModel.userName.value.isEmpty &&
-                      bind.mainGetLocalOption(key: "show-scam-warning") != "N"
-                  ? () => showScamWarning(context, serverModel)
-                  : serverModel.toggleService),
+              serverModel.toggleService),
           PermissionRow(translate("Input Control"), serverModel.inputOk,
               serverModel.toggleInput),
           // PermissionRow(translate("Transfer file"), serverModel.fileOk2,
